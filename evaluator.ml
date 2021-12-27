e946070f29dc3a36c049e7e4a1dc7b74ffd8f59e
DrNatoor <db2002dominic@gmail.com>
2021-12-27 10:53:08 +0100

    init evaluator and type_checker

---
+++ evaluator.ml
@@ -0,0 +1,5 @@
+(* imports *)
+open Types
+
+(** EVALUATOR - computes the value of an expression in an environment *)
+let evaluator ((expression: expr), (environment: 'a list)): string = "TODO: implement evaluator"
---
+++ type_checker.ml
@@ -0,0 +1,5 @@
+(* imports *)
+open Types
+
+(** TYPE CHECKER - computes the type of an expression in an environment *)
+let type_checker (expression: expr): 'a = "TODO: implement type checker"
