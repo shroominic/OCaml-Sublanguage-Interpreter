95cd8c01e2218a066d58f75370044066e23d54b4
DrNatoor <db2002dominic@gmail.com>
2021-12-27 10:54:32 +0100

    comment

--- parser.ml
+++ parser.ml
@@ -1,4 +1,5 @@
+(* imports *)
 open Types
 
 (** PARSER - translates list of tokens into abstract expressions *)
-let parser (list_of_tokens: string list): expr = Var "TODO: parser"
+let parser (list_of_tokens: string list): expr = Var "TODO: implement parser"
