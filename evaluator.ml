c4f7388d5a2593c216481c136deed997495da94f
DrNatoor <db2002dominic@gmail.com>
2021-12-28 08:46:46 +0100

    eval function done

--- evaluator.ml
+++ evaluator.ml
@@ -1,8 +1,6 @@
 (* imports *)
 open Grammar
 
-let todo_implement = IntVal 42
-
 (** EVALUATOR - computes the value of an expression in an environment *)
 let eval env e : value = 
   match e with
@@ -16,7 +14,7 @@ let eval env e : value =
   | LmdaTy (x, _, e)            -> Closure (x, e, env)
   | Let    (x, e1, e2)          -> eval (update env x (eval env e1)) e2
   | Letrec (f, x, e1, e2) 
-  | LetRTy (f, x, _, _, e1, e2) -> todo_implement
+  | LetRTy (f, x, _, _, e1, e2) -> eval (update env f Rclosure(f, x, e1, env)) e2
 and eval_var env v = 
   let unwrap = lookup env v in 
   match unwrap with 
@@ -29,10 +27,16 @@ and eval_op o v1 v2 =
   | Mult,   IntCon(x), IntCon(y) -> IntCon(x * y)
   | LessEq, IntCon(x), IntCon(y) -> BoolCon(x <= y)
   | _, _, _ -> failwith "operator application not supported"
-and eval_fn v1 v2 = todo_implement 
-and eval_if v1 v2 v3 = todo_implement
-
-
+and eval_fn v1 v2 = 
+  match v1 with
+  | Closure (x, e, env') -> eval (update env' x v2) e
+  | Rclosure(f, x, e, env') -> eval (update (update env' f v1) x v2) e 
+  | _ -> failwith "function can't be evaluated"
+and eval_if v1 v2 v3 = 
+  match v1 with
+  | BoolCon(true) -> v2
+  | BoolCon(false) -> v3
+  | _ -> failwith "if statement can't be evaluated"
 
 (** testing *)
 let () = Test.evaluation_test ()
