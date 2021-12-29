5601ba7b288626819a49ed4bc2df31616ba53f66
DrNatoor <db2002dominic@gmail.com>
2021-12-29 21:35:21 +0100

    cleanup

--- evaluator.ml
+++ evaluator.ml
@@ -8,43 +8,43 @@ let rec eval env e : value =
   | Con    (BoolCon b)          -> BoolVal b 
   | Con    (IntCon i)           -> IntVal i
   | OpApp  (o, e1, e2)          -> eval_op o (eval env e1) (eval env e2)
-  | FnApp  (e1, e2)             -> eval_fn (eval env e1) (eval env e2)
+  | FnApp  (e1, e2)             -> eval_fun (eval env e1) (eval env e2)
   | If     (e1, e2, e3)         -> eval_if env e1 e2 e3
   | Lmda   (x, e)
   | LmdaTy (x, _, e)            -> Closure (x, e, env)
   | Let    (x, e1, e2)          -> eval (update env x (eval env e1)) e2
   | LetR   (f, x, e1, e2) 
   | LetRTy (f, x, _, _, e1, e2) -> eval (update env f (Rclosure (f, x, e1, env))) e2 
-(**  *)
+(** variable evaluation *)
 and eval_var env v = 
   let unwrap = lookup env v in 
   match unwrap with 
   | Some v -> v 
-  | None   -> failwith "missing variable"
-(**  *)
+  | None   -> failwith "eval_var: unbound variable"
+(** operator evaluation *)
 and eval_op o v1 v2 = 
   match o, v1, v2 with 
   | Add,    IntVal(x), IntVal(y) -> IntVal(x + y)
   | Sub,    IntVal(x), IntVal(y) -> IntVal(x - y)
   | Mul,    IntVal(x), IntVal(y) -> IntVal(x * y)
   | LessEq, IntVal(x), IntVal(y) -> BoolVal(x <= y)
-  | _, _, _ -> failwith "operator application not supported"
-(**  *)
-and eval_fn v1 v2 = 
+  | _, _, _ -> failwith "eval_op_app: ill-typed arguments"
+(** function evaluation *)
+and eval_fun v1 v2 = 
   match v1 with
-  | Closure (x, e, env') -> eval (update env' x v2) e
-  | Rclosure(f, x, e, env') -> eval (update (update env' f v1) x v2) e 
-  | _ -> failwith "function can't be evaluated"
-(**  *)
+  | Closure (x, e, env) -> eval (update env x v2) e
+  | Rclosure(f, x, e, env) -> eval (update (update env f v1) x v2) e 
+  | _ -> failwith "eval_fun_app: function expected"
+(** if-then-else evaluation *)
 and eval_if env e1 e2 e3 = 
   let v1 = eval env e1 in
   match v1 with
   | BoolVal(true) -> eval env e2
   | BoolVal(false) -> eval env e3
-  | _ -> failwith "if statement can't be evaluated"
+  | _ -> failwith "eval_if: boolean expected"
 
 
-(**   DEBUG AND TESTING   *)
+(**   TESTING   *)
 
 (** test input *)
 let input: int = 17
