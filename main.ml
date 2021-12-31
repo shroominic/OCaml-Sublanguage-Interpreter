76a20ef079d8512feb69829b894da59e33c17c9f
DrNatoor <db2002dominic@gmail.com>
2021-12-31 17:44:39 +0100

    implement interpreter

--- main.ml
+++ main.ml
@@ -4,6 +4,37 @@ LEXER -> PARSER -> TYPE CHECKER -> EVALUATOR
 *)
 
 (* imports *)
+open Grammar
+open Lexer
+open Parser
+open Type_checker
+open Evaluator
 
-(** MAIN *)
-let main () = print_endline "TODO: main"
+(** input string - factorial(17) *)
+let fac_string =
+  " let rec fac a = fun n ->     \n\
+  \     if n <= 1                \n\
+  \     then a                   \n\
+  \     else fac (n*a) (n-1) in  \n\
+  \     fac 1 17 "
+
+(** parsing *)
+let lexed_string = lex fac_string
+let parsed_string = parser lexed_string
+
+(** typecheck string: string -> type *)
+let check_string str : btype = check empty parsed_string
+
+(** evaluate string: string -> value *)
+let eval_string str : value = eval empty parsed_string
+
+let unwrap_int_value v : int = 
+  match v with
+  | IntVal (x) -> x
+  | _ -> failwith "unable to unwrap type"
+
+(** result of factorial(17) -> int *)
+let result = unwrap_int_value (eval_string fac_string)
+
+(** print the final result *)
+let () = print_endline ("Factorial(17): " ^ (string_of_int result))
